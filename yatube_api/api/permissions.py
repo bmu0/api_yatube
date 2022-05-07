3f9cf408dee0c2816f0a19ef4dcbca97048269a7
Максим Близнюк <bmu0@yandex.ru>
2022-05-07 20:19:25 +0300

    second try

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -5,7 +5,7 @@ class IsAuthorOrIsAuthenticated(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return (
-            obj.author == request.user 
+            obj.author == request.user
             if request.method not in SAFE_METHODS
             else bool(request.user and request.user.is_authenticated)
         )
